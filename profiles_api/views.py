3ec79447936181d128d37ea15c73305f2503f3bf
Corey Coole <coreycoole@gmail.com>
2019-11-30 14:16:18 -0700

    update ApiViews

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 
 
-class HelloAPIView(APIView):
+class HelloApiView(APIView):
     """Test API View"""
 
     def get(self, request, format=None):
